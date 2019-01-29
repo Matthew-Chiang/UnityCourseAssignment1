d5cc92c849fb08a3ba81c6fffe4768c1ab818d5d
Matthew <matthew.chiang.myc@gmail.com>
2019-01-29 08:56:43 -0500

    first

---
+++ Assets/camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    public Camera firstPersonCamera;
+    public Camera overheadCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+      firstPersonCamera.enabled = false;
+      overheadCamera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+          firstPersonCamera.enabled = !firstPersonCamera.enabled;
+          overheadCamera.enabled = !overheadCamera.enabled;
+        }
+    }
+}
---
+++ Assets/leftClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+      transform.Rotate(0,10,0);
+    }
+}
